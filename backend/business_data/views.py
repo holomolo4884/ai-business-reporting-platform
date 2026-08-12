48ded3e056e85ddfed7a024eb1389660664f1c35
holomolo4884 <goncharov.vlad2006@gmail.com>
2026-08-13 03:18:04 +0700

    F-08. Сделать CRUD API для Expenses

--- backend/business_data/views.py
+++ backend/business_data/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from business_data.models import Order
+from business_data.models import Expense, Order
 from business_data.serializers import (
+    ExpenseCreateSerializer,
+    ExpenseSerializer,
+    ExpenseUpdateSerializer,
     OrderCreateSerializer,
     OrderSerializer,
     OrderUpdateSerializer,
@@ -27,3 +30,23 @@ class OrderViewSet(viewsets.ModelViewSet):
         if self.action in ("update", "partial_update"):
             return OrderUpdateSerializer
         return OrderSerializer
+
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    """ViewSet для управления расходами."""
+
+    permission_classes = [IsAuthenticated]
+    ordering_fields = ["expense_date", "amount", "created_at"]
+    ordering = ["-expense_date"]
+
+    def get_queryset(self):
+        # Пока возвращаем все расходы.
+        # Ограничение по организациям добавим в F-11.
+        return Expense.objects.select_related("organization")
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return ExpenseCreateSerializer
+        if self.action in ("update", "partial_update"):
+            return ExpenseUpdateSerializer
+        return ExpenseSerializer
